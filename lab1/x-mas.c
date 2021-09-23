507e2877fbdbb37224730db7cdb357b3f9feee4c
anshumankomawar <anshumankomawar@gmail.com>
2021-09-22 23:07:52 -0700

    Added lab1 specs

---
+++ lab1/x-mas.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  int height = -1;
+
+  while(height < 0 || height > 10) {
+    printf("Enter the height of the tree: ");
+    scanf("%d", &height);
+  }
+
+  int level = 0;
+  
+  while(level < 0) {
+    int spaces = ;
+    int stars = ;
+    
+    for(int leftspace = 0; leftspace <= ; leftspace++) {
+      printf(" ");
+    }
+
+    while(stars > 0) {
+    }
+
+    while(spaces > 0) {
+      printf(" ");
+    }
+
+    printf();
+  }
+
+  printf("%d\n", height);
+  return 0;
+}
